2eb832cdf08b1466adf0583a535a7ac918465dc8
Alex Pirjol <alex.pirjol@gmail.com>
2013-03-02 13:24:57 +0200

    page 83
    unit test works

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "product attributes must not be empty" do
+    product = Product.new
+    assert product.invalid?, 'product is invalid'
+    assert product.errors[:title].any?, 'title has errors'
+    assert product.errors[:description].any?, 'description has errors'
+    assert product.errors[:image_url].any?, 'image_url has errors'
+  end
 end
